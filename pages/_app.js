df1fe908d7c2a9980891d6ca57b8c86c006ba5f4
John <jooohn.eth@gmail.com>
2022-07-07 13:26:05 -0400

    Setting up functions for wallet connections

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,37 @@ import 'easymde/dist/easymde.min.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const [account, setAccount] = useState(null);
+
+  async function getWeb3Modal() {
+    const web3Modal = new Web3Modal({
+      cacheProvider: false,
+      providerOptions: {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: { 
+            infuraId: "42e4bb6840fd45ac87a53be66b381b93"
+          },
+        },
+      },
+    })
+    return web3Modal
+  }
+
+  async function connect() {
+    try {
+      const web3Modal = await getWeb3Modal()
+      const connection = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(connection)
+      const accounts = await provider.listAccounts()
+      setAccount(accounts[0])
+    } catch (err) {
+      console.log('error:', err)
+    }
+  }
+
+
 }
 
 export default MyApp
