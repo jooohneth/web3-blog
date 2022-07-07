342fb4a8ea9b19280f78a3f388e369d117dcb511
John <jooohn.eth@gmail.com>
2022-07-07 13:48:31 -0400

    Added CSS to navigation

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -89,4 +89,72 @@ function MyApp({ Component, pageProps }) {
 
 }
 
+const accountInfo = css`
+  width: 100%;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  font-size: 12px;
+`
+
+const container = css`
+  padding: 40px;
+`
+
+const linkContainer = css`
+  padding: 30px 60px;
+  background-color: #fafafa;
+`
+
+const nav = css`
+  background-color: white;
+`
+
+const header = css`
+  display: flex;
+  border-bottom: 1px solid rgba(0, 0, 0, .075);
+  padding: 20px 30px;
+`
+
+const description = css`
+  margin: 0;
+  color: #999999;
+`
+
+const titleContainer = css`
+  display: flex;
+  flex-direction: column;
+  padding-left: 15px;
+`
+
+const title = css`
+  margin-left: 30px;
+  font-weight: 500;
+  margin: 0;
+`
+
+const buttonContainer = css`
+  width: 100%;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+`
+
+const buttonStyle = css`
+  background-color: #fafafa;
+  outline: none;
+  border: none;
+  font-size: 18px;
+  padding: 16px 70px;
+  border-radius: 15px;
+  cursor: pointer;
+  box-shadow: 7px 7px rgba(0, 0, 0, .1);
+`
+
+const link = css`
+  margin: 0px 40px 0px 0px;
+  font-size: 16px;
+  font-weight: 400;
+`
+
 export default MyApp
