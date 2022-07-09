7b9b691bc4fb36c5c4d60e95494f86461fb8f751
John <jooohn.eth@gmail.com>
2022-07-09 11:13:04 -0400

    Added hooks and variables

--- pages/create-post.jsx
+++ pages/create-post.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react' // new
+import { useState, useRef, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
 import { css } from '@emotion/css'
@@ -6,3 +6,27 @@ import { ethers } from 'ethers'
 import { create } from 'ipfs-http-client'
 import { contractAddress } from '../config'
 import Blog from '../artifacts/contracts/Blog.sol/Blog.json'
+
+const client = create('https://ipfs.infura.io:5001/api/v0')
+const SimpleMDE = dynamic(
+  () => import('react-simplemde-editor'),
+  { ssr: false }
+)
+const initialState = { title: '', content: '' }
+
+function CreatePost() {
+  const [post, setPost] = useState(initialState)
+  const [image, setImage] = useState(null)
+  const [loaded, setLoaded] = useState(false)
+  const fileRef = useRef(null)
+  const { title, content } = post
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true)
+    }, 500)
+  }, [])
+}
+
+export default CreatePost
