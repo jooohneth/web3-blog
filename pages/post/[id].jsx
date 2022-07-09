453933872031931849c10af603c8bae53ef3c167
John <jooohn.eth@gmail.com>
2022-07-09 12:00:02 -0400

    Setup variables and added JSX for UI

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -8,3 +8,39 @@ import { AccountContext } from '../../context'
 import { contractAddress, ownerAddress } from '../../config'
 import Blog from '../../artifacts/contracts/Blog.sol/Blog.json'
 
+function Post({post}){
+    const account = useContext(AccountContext);
+    const router = useRouter();
+    const {id} = router.query;
+    
+    if(router.isFallback){
+        return <div>Loading...</div>
+    }
+
+    return(
+        <div>
+            {post && (
+                <div className={container}>
+                    {ownerAddress === account && (
+                        <div className={editPost}>
+                            <Link href={`/edit-post/${id}`}>
+                                <a>
+                                    Edit Post
+                                </a>
+                            </Link>
+                        </div>
+                    )}
+                    {post.coverImage && (
+                        <img src={post.coverImage} className={coverImageStyle}/>
+                    )}
+                    <h1>{post.title}</h1>
+                    <div className={contentContainer}>
+                        <ReactMarkdown>{post.content}</ReactMarkdown>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Post;
