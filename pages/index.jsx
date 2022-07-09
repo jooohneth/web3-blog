a0580fbb66b1c34e25770dd46119e0683ce24c02
John <jooohn.eth@gmail.com>
2022-07-09 11:09:54 -0400

    Fixing a few bugs

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,47 +5,54 @@ import { ethers } from 'ethers';
 import Link from 'next/link';
 import { AccountContext } from '../context';
 import { contractAddress, ownerAddress } from '../config';
-import Blog from "../artifacts/contracts/Blog.sol/Blog.json";
+import Blog from '../artifacts/contracts/Blog.sol/Blog.json';
 
-export default function Home(props){
-  const {posts} = props;
-  const account = useContext(AccountContext);
-  const router = useRouter();
-
-  async function navigate(){
-    router.push("/create-post");
+export default function Home(props) {
+  const { posts } = props
+  const account = useContext(AccountContext)
+  const router = useRouter()
+  async function navigate() {
+    router.push('/create-post')
   }
 
   return (
     <div>
       <div className={postList}>
-        {posts.map((post,index) => {
-          <Link href={`/post/${post[2]}`} >
-            <a>
-              <div className={linkStyle}>
-                <p className={postTitle}>{post[1]}</p>
-                <div className={arrowContainer}>
-                  <img className={smallArrow} src="/right-arrow.svg" alt="Right arrow" />
+        {posts.map((post, index) => (
+            <Link href={`/post/${post[2]}`} key={index}>
+              <a>
+                <div className={linkStyle}>
+                  <p className={postTitle}>{post[1]}</p>
+                  <div className={arrowContainer}>
+                  <img
+                      src='/right-arrow.svg'
+                      alt='Right arrow'
+                      className={smallArrow}
+                    />
+                  </div>
                 </div>
-              </div>
-            </a>
-          </Link>
-        })}
+              </a>
+            </Link>
+          ))}
       </div>
       <div className={container}>
         {(account === ownerAddress) && posts && !posts.length && (
-          <button className={buttonStyle}>
-            Create your first post
-            <img src="/right-arrow.svg" alt="Right Arrow" className={arrow} />
-          </button>
-        )}
+            <button className={buttonStyle} onClick={navigate}>
+              Create your first post
+              <img
+                src='/right-arrow.svg'
+                alt='Right arrow'
+                className={arrow}
+              />
+            </button>
+          )}
       </div>
     </div>
   )
-} 
+}
 
-export async function getServerSideProps(){
-  let provider;
+export async function getServerSideProps() {
+  let provider
   if (process.env.ENVIRONMENT === 'local') {
     provider = new ethers.providers.JsonRpcProvider()
   } else if (process.env.ENVIRONMENT === 'testnet') {
@@ -54,15 +61,13 @@ export async function getServerSideProps(){
     provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/')
   }
 
-  const contract = new ethers.Contract(contractAddress, Blog.abi, provider);
-
+  const contract = new ethers.Contract(contractAddress, Blog.abi, provider)
   const data = await contract.fetchPosts()
   return {
     props: {
       posts: JSON.parse(JSON.stringify(data))
     }
   }
-
 }
 
 const arrowContainer = css`
