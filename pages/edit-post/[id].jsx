3198bc7678a4263139e87e7b86b227e9465a36ed
John <jooohn.eth@gmail.com>
2022-07-09 13:16:31 -0400

    Initailized variables and state

--- pages/edit-post/[id].jsx
+++ pages/edit-post/[id].jsx
@@ -8,3 +8,24 @@ import { create } from 'ipfs-http-client'
 import { contractAddress } from '../../config'
 import Blog from '../../artifacts/contracts/Blog.sol/Blog.json'
 
+const ipfsURI = 'https://ipfs.io/ipfs/'
+const client = create('https://ipfs.infura.io:5001/api/v0')
+const SimpleMDE = dynamic(
+    () => import('react-simplemde-editor'),
+    { ssr: false }
+  )
+
+function Post(){
+    const [post, setPost] = useState(null);
+    const [editing, setEditing] = useState(true);
+    const router = useRouter();
+    const id = router.query;
+
+    useEffect(()=>{
+        fetchPost();
+    }, [id])
+
+}
+
+export default Post;
+  
