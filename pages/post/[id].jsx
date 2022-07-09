5d1a09f0e7bac3e8f99f6389b3138423b2918096
John <jooohn.eth@gmail.com>
2022-07-09 11:39:07 -0400

    Imports

---
+++ pages/post/[id].jsx
@@ -0,0 +1,10 @@
+import ReactMarkdown from 'react-markdown'
+import { useContext } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { css } from '@emotion/css'
+import { ethers } from 'ethers'
+import { AccountContext } from '../../context'
+import { contractAddress, ownerAddress } from '../../config'
+import Blog from '../../artifacts/contracts/Blog.sol/Blog.json'
+
