6cc7a254ea28d773b057dde441ba1f7fcde7947f
John <jooohn.eth@gmail.com>
2022-07-09 12:23:48 -0400

    Added styling

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -82,4 +82,27 @@ export async function getStaticProps({ params }) {
   }
 }
 
+const editPost = css`
+  margin: 20px 0px;
+`
+
+const coverImageStyle = css`
+  width: 900px;
+`
+
+const container = css`
+  width: 900px;
+  margin: 0 auto;
+`
+
+const contentContainer = css`
+  margin-top: 60px;
+  padding: 0px 40px;
+  border-left: 1px solid #e7e7e7;
+  border-right: 1px solid #e7e7e7;
+  & img {
+    max-width: 900px;
+  }
+`
+
 export default Post;
