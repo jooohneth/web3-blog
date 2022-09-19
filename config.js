e73badd0c58d708a51d450aa2896ff227aa3e9af
John <jooohn.eth@gmail.com>
2022-09-19 16:10:00 -0400

    addresses

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0xfB4D83E7d1286Ff204d8B9b4f3D5035A86d8247A"
+  export const contractAddress = "0x30D3bF1eb800DC48376E2062ed14921E34c88507"
   export const ownerAddress = "0x7947C1c9D14Ade004Bd7f8Da1C94c49AAEed02ee"
   
