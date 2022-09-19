f8048bd6a9051846b25cf84986b8ffd271468e38
John <jooohn.eth@gmail.com>
2022-09-19 16:08:02 -0400

    Modified for env variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    //hardhat: {
-    //  chainId: 1337
-    //},
-     mumbai: {
-       url: process.env.MUMBAI_RPC,
-       accounts: [process.env.PRIVATE_MUMBAI_KEY]
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.NEXT_PUBLIC_MUMBAI_RPC,
+      accounts: [process.env.NEXT_PUBLIC_PRIVATE_MUMBAI_KEY],
     },
     // polygon: {
     //   url: "https://polygon-rpc.com/",
     //   accounts: [process.env.pk]
     // }
-  }
+  },
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+require("dotenv").config();
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
+    MUMBAI_RPC: process.env.NEXT_PUBLIC_MUMBAI_RPC,
+    PRIVATE_MUMBAI_KEY: process.env.NEXT_PUBLIC_PRIVATE_MUMBAI_KEY,
+  },
+};
